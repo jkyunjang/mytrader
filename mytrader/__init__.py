31943e8bd54e3387f8c18c0eac502bb5dbb1e134
jkyunjang <jkyunjang.gmail.com>
2024-10-28 20:24:45 +0900

    delete DOTENV_REQUIRED

--- mytrader/__init__.py
+++ mytrader/__init__.py
@@ -2,7 +2,7 @@ __version__ = "0.0.1"
 __author__ = "jkyunjang"
 
 # required in .env file
-DOTENV_REQUIRED = [
-    'UPBIT_ACCESS_KEY',
-    'UPBIT_SECRET_KEY'
-]
+# DOTENV_REQUIRED = [
+#     'UPBIT_ACCESS_KEY',
+#     'UPBIT_SECRET_KEY'
+# ]
